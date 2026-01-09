77eb5b5eb46a43f06af1632b93248abd7a28267c
Bennymens <Benedictmensah235@yahoo.com>
2026-01-08 16:35:42 -0800

    Add STATIC_ROOT setting for Render deployment

--- impactvate/settings.py
+++ impactvate/settings.py
@@ -122,6 +122,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Add this line
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
